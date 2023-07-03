2d6514044550d416e4f4b02afdcd1d50c813e709
Ankit Singh <ankitsingh5940@gmail.com>
2023-07-03 16:27:15 +0530

    Added Animations And input Changes
    added basic movement and jumping animations to the character and changed the collider parent to the 3d mesh hip bone(Root bone) so collider can follow anywhere the player moves or animates. And changes the input and camera rotation and made it less lines with unity math functions in Movement scripts.

--- Parkour_World/Assets/Scripts/MovementScript.cs
+++ Parkour_World/Assets/Scripts/MovementScript.cs
@@ -1,64 +1,141 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
-using static UnityEditor.Timeline.TimelinePlaybackControls;
 
 public class MovementScript : MonoBehaviour
 {
-    // rayCast will touch ground from half player height to halt when movement key releases
-    public float groundDrag;
-    [Header("Ground Check")]
-    public float playerHeight;
-    public LayerMask whatIsGround; // 0.8 is working
-    bool grounded;
-    public float rayCastHeight;
-
-    [Header("Player Speed & Orientation")]
+    // Adjustables from editor
+    [Header("Movements")]
     public float playerSpeed;
-    public Transform orient;
+    public float jumpHeight;
+    public float airVelocity;
+    public float groundDrag; // rigidBody to control drag according to ground check
 
-    float horizontalInput;
-    float verticalInput;
+    [Header("Cam & Key Rotations")]
+    public new Transform camera;
+    public float turnSmoothTime;
+    public float smoothVel;
 
-    Vector3 moveDirection;
+    // trigger for ground check from collision functions
+    public bool grounded;
     private Rigidbody rb;
+
+    // Input variables
+    float horizontalInput; // gets axis
+    float verticalInput;   // gets axis
+    Vector3 movement; // in which dir. should a player be moving according to inputs
+
+    // Animation
+    Animator animator;
+    int animXposID;
+    int animZposID;
+    int animJump;
+    public float animJumpTransitionTime = 0.15f;
+
 
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+        // setting value from animation Editor to code userBased named ID
+        animXposID=Animator.StringToHash("moveX");
+        animZposID = Animator.StringToHash("moveZ");
+        animJump = Animator.StringToHash("Jump");
     }
 
     private void Update()
     {
-        Vector3 position = transform.position;
-        grounded = Physics.Raycast(position, Vector3.down, playerHeight * rayCastHeight + 0.3f, whatIsGround);
+        movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
 
-        myInput();
-        Debug.DrawRay(transform.position + new Vector3(0, 0.05f, 0), Vector3.down, Color.blue);
+        // control the velocity of rigidDoby if collision touches ground layer then ground drag will preform
         if (grounded)
         {
-            Debug.Log(grounded);
             rb.drag = groundDrag;
         }
         else
+            // drag becomes zero and prevents player from slow motion jump/downfall
             rb.drag = 0;
     }
 
     private void FixedUpdate()
     {
-        movePlayer();
+        // if player speed not in sync with rigidBody physics then cap to speed
+        speedControl();
+        // for player to rotate with input and camera direction
+        movePlayer(movement, grounded);
+        // handle playerInputs and Animations
+        playerInputAndAnimations(movement, grounded);
     }
 
-    void myInput()
+    void movePlayer(Vector3 direction, bool isOnGround)
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        verticalInput = Input.GetAxis("Vertical");
+        // if player is touching the ground then only can rotate
+        if (isOnGround && direction.magnitude > 0.01f)
+        {
+            // get the angle & set the player face to input and camera direction
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + camera.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref smoothVel, turnSmoothTime);
+
+            // rotate the face of player
+            rb.rotation = Quaternion.Euler(0, angle, 0);
+
+            // face in the camera direction and move there
+            Vector3 camDirection = Quaternion.Euler(0, angle, 0) * Vector3.forward;
+            rb.AddForce(camDirection.normalized * playerSpeed, ForceMode.Force);
+        }
     }
 
-    void movePlayer()
+    void playerInputAndAnimations(Vector3 playerDirection, bool isOnGround)
     {
-        moveDirection = moveDirection = orient.forward * verticalInput + orient.right * horizontalInput;
-        rb.AddForce(moveDirection.normalized * playerSpeed, ForceMode.Force);
+        // player jump
+        if (isOnGround)
+        {
+            // Animations
+            animator.SetFloat(animXposID, playerDirection.x);
+            animator.SetFloat(animZposID, playerDirection.z);
+
+            if (Input.GetKey(KeyCode.Space))
+            {
+                animator.CrossFade(animJump, animJumpTransitionTime);
+
+                rb.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
+            }
+        }
+        // if player is in air or jumping then player should not have regular velocity of movement
+        // and stop all strafing animations
+        else if (!isOnGround)
+        {
+            // Animation stop
+            animator.SetFloat(animXposID, 0);
+            animator.SetFloat(animZposID, 0);
+
+            rb.AddForce(playerDirection * airVelocity, ForceMode.Force);
+        }
+    }
+
+    // get grounded bool true or false from these triggerEnter on specific tags
+    private void OnTriggerEnter(Collider trigger)
+    {
+        if (trigger.tag == "Ground")
+        {
+            grounded = true;
+        }
+    }
+    private void OnTriggerExit(Collider trigger)
+    {
+        grounded = false;
+    }
+
+    void speedControl()
+    {
+        // taking velocity from rigidbody and storing it in flatVel. to get previous velocity of player
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+
+        // limit the speed
+        if (flatVel.magnitude > playerSpeed)
+        {
+            Vector3 limitSpeed = flatVel.normalized * playerSpeed;
+            rb.velocity = new Vector3(limitSpeed.x, rb.velocity.y, limitSpeed.z);
+        }
     }
 }
